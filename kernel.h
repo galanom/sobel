b6fd21d669906a0b9b3b1d62d213eb0c995af18b
Ioannis Galanommatis <galanom@gmail.com>
2024-08-03 01:55:48 +0300

    Add kernel.h, the sobel filter kernel

---
+++ kernel.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define Gx_00 -1
+#define Gx_01 0
+#define Gx_02 1
+#define Gx_10 -2
+#define Gx_11 0
+#define Gx_12 2
+#define Gx_20 -1
+#define Gx_21 0
+#define Gx_22 1
+
+#define Gy_00 -1
+#define Gy_01 -2
+#define Gy_02 -1
+#define Gy_10 0
+#define Gy_11 0
+#define Gy_12 0
+#define Gy_20 1
+#define Gy_21 2
+#define Gy_22 1
